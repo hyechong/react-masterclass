a6fdf2b3f42246e2cf47504acc5e2ccef3f8b167
hyechong <ckgpwl2@gmail.com>
2023-08-01 06:07:50 +0900

    styled components 0801

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const animation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
 `;
 
 const Box = styled.div`
-  background: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${animation} 1s linear infinite;
+  ${Emoji} {
+    &:hover {
+      font-size: 100px;
+    }
+    transition: all 0.4s;
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor='teal' />
-      <Circle bgColor='lightblue' />
-    </Father>
+    <Wrapper>
+      <Box>
+        <Emoji as='p'>🤩</Emoji>
+      </Box>
+      <Emoji>😈</Emoji>
+    </Wrapper>
   );
 }
 
