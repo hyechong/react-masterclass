7442315a3cb6492e17efa43e9d5fe65089311bca
hyechong <ckgpwl2@gmail.com>
2023-08-03 21:07:40 +0900

    typescript 0803

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <h1>{text}</h1>;
+}
+function App() {
+  const onClick = (event: React.FormEvent<HTMLButtonElement>) => {};
+  return (
+    <Container>
+      <Dummy active text='hello' />
+      <form>
+        <button onClick={onClick}>click me</button>
+      </form>
+    </Container>
+  );
+}
+
+export default App;
