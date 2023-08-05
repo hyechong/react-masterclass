e0f9d7fc67dd28b79f5676306375ba9486b01121
hyechong <ckgpwl2@gmail.com>
2023-08-05 20:43:43 +0900

    typescript0805

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
-import { useState } from 'react';
-import { styled } from 'styled-components';
+import Router from './Router';
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-interface DummyProps {
-  text: string;
-  active?: boolean;
-}
-
-function Dummy({ text, active = false }: DummyProps) {
-  return <h1>{text}</h1>;
-}
 function App() {
-  const onClick = (event: React.FormEvent<HTMLButtonElement>) => {};
-  return (
-    <Container>
-      <Dummy active text='hello' />
-      <form>
-        <button onClick={onClick}>click me</button>
-      </form>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
