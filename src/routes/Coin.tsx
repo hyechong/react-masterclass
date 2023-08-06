597a30fc5c6062ec71c761dac4659e9978a3b59c
hyechong <ckgpwl2@gmail.com>
2023-08-06 10:36:51 +0900

    coin 0806

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useParams } from 'react-router';
+
+const Coin = () => {
+  const { coinId } = useParams();
+  return <div>Coin: {coinId}</div>;
+};
+
+export default Coin;
